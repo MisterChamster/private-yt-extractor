4f9ad5ac328f2462dab22c2580feca4a0099b1b7
Mr Chamster <s.duda03@gmail.com>
2025-07-06 00:20:31 +0200

    ask_round_or_exact update

--- src/module_askers.py
+++ src/module_askers.py
@@ -6,6 +6,25 @@ def ask_url():
     return url
 
 
+
+def ask_round_or_exact():
+    """
+    Asks user if extracted video views should be exact or rounded.
+
+    Returns:
+        str: "round" or "exact".
+    """
+    input_RE = " "
+    RE_dict = {"r": "round", "e": "exact"}
+    print("Choose view count type (exact values will take more time):\n" \
+          "r - rounded\n" \
+          "e - exact\n\n>> ", end="")
+    input_RE = str(input())
+
+    if input_RE in RE_dict:
+        return RE_dict[input_RE]
+
+
 def ask_extract_write_order():
     """
     Asks user for extract order.
@@ -16,20 +35,5 @@ def ask_extract_write_order():
     order = ""
     result_dict = {"a": "asc", "d": "desc"}
     while order not in result_dict:
-        order = input("In what order do You want to write elements to file? (a - ascending, d - descending)\n>>").lower()
+        order = input("In what order do You want to write elements to file? (a - ascending, d - descending)\n>>" )
     return result_dict[order]
-
-
-def ask_round_or_exact():
-    """
-    Asks user if extracted video views should be exact or rounded.
-
-    Returns:
-        str: "round" or "exact".
-    """
-    input_RE = " "
-    RE_dict = {"": "round", "r": "round", "e": "exact"}
-    input_RE = input("Do You want viewcount on every video be exact or rounded? Extracting exact values will take more time. (Enter - rounded, e - exact)\n>>").lower()
-
-    if input_RE in RE_dict:
-        return RE_dict[input_RE]
